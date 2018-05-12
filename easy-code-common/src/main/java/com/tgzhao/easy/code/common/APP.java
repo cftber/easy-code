24b298547caccbafec496aa15e890b4a24882dd4
tgzhao <tgzhao@Ctrip.com>
2018-05-12 10:02:00 +0800

    init easycode project

---
+++ easy-code-common/src/main/java/com/tgzhao/easy/code/common/APP.java
@@ -0,0 +1,11 @@
+package com.tgzhao.easy.code.common;
+
+/**
+ * @author tgzhao
+ * @since 2018/5/12
+ */
+public class APP {
+  public static void main(String[] args) {
+
+  }
+}
---
+++ easy-code-common/src/main/java/com/tgzhao/easy/code/common/rxjava/APP.java
@@ -0,0 +1,11 @@
+package com.tgzhao.easy.code.common.rxjava;
+
+/**
+ * @author tgzhao
+ * @since 2018/5/12
+ */
+public class APP {
+  public static void main(String[] args) {
+
+  }
+}
